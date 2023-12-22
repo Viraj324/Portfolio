7bb935a702978b902d091d1145cd3c410b6030cf
Viraj <viraj.sonawane22@pccoepune.org>
2023-12-22 14:15:45 +0530

    npm build run

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Viraj Sonawane's Portfolio
+            Viraj Sonawane Portfolio
           </h1>
         </motion.div>
 
